b6f5c44a8facbe74e405a7d06369d3e10722ab4e
WickedZ2309 <168111978+WickedZ2309@users.noreply.github.com>
2024-06-17 17:20:51 -0400

    added the current location

--- js/index.js
+++ js/index.js
@@ -28,6 +28,9 @@ function updateTime() {
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
@@ -39,7 +42,7 @@ function updateCity(event) {
       <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
     </div>
       <div class="time">
-        ${cityTime.format("h:mm:ss")}<small>${cityTime.format("A")}</small>
+        ${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")}</small>
       </div>
   </div>`;
 }
